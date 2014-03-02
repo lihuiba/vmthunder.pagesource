2514b71738f93283cb92ec325e276231eda16026
Huiba Li <lihuiba@gmail.com>
2014-03-02 23:26:11 +0800

    source

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/VMThunder/"
+http_images_path = "/VMThunder/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/VMThunder/fonts"
+css_dir = "public/VMThunder/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
